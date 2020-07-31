6816c5cc1b6600888af325506f3e951e4058eb22
Nicolas Ayala <ayalanick84@gmail.com>
2020-07-30 23:33:26 -0700

    Updates

---
+++ precache-manifest.902f18c39e1f64b747b619d71c5aa87d.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a5dc435ad08da74d3a7581431a49a16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "415d35cded7370f18867",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7e5c36cdebc3225823f2",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "415d35cded7370f18867",
+    "url": "/static/js/2.2495994c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.2495994c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e5c36cdebc3225823f2",
+    "url": "/static/js/main.649eb152.chunk.js"
+  },
+  {
+    "revision": "2832ad4f15d647ebd0d0",
+    "url": "/static/js/runtime-main.f11473f4.js"
+  },
+  {
+    "revision": "a5c80dbafeee8be86409afe687c93201",
+    "url": "/static/media/lighthouse.a5c80dba.jpg"
+  },
+  {
+    "revision": "32bd354775aedf69fd447029d95a3ca4",
+    "url": "/static/media/mariachi.32bd3547.jpg"
+  },
+  {
+    "revision": "0cf114e098dba54fbe156a745b72d668",
+    "url": "/static/media/monitor.0cf114e0.jpg"
+  },
+  {
+    "revision": "e5f32c5ee20c0685fcaaebd75e573d2b",
+    "url": "/static/media/moodies-start.e5f32c5e.png"
+  },
+  {
+    "revision": "fdce11f70bcff032939cb57f2354066f",
+    "url": "/static/media/note.fdce11f7.png"
+  },
+  {
+    "revision": "a12aca277f8d5db1ab8d7f5fe03fe5f5",
+    "url": "/static/media/postcard.a12aca27.jpg"
+  },
+  {
+    "revision": "388f464e4af732ea1dcb10998b3ea5de",
+    "url": "/static/media/quaranscreen1.388f464e.png"
+  },
+  {
+    "revision": "02dbae8efd3784196969d575ea6d3dc1",
+    "url": "/static/media/rendezview.02dbae8e.png"
+  },
+  {
+    "revision": "d9a3a10f24556758c8f44e3511ea1be9",
+    "url": "/static/media/resume.d9a3a10f.pdf"
+  },
+  {
+    "revision": "cd9251055b181358aa2e798c5d724f2d",
+    "url": "/static/media/selfie.cd925105.jpg"
+  },
+  {
+    "revision": "a857c3836d4ece1b88dcf421df854820",
+    "url": "/static/media/team.a857c383.jpg"
+  }
+]);
